6f38daeef93a1aec8ff49da3c3d8eefebc5f2212
Ferhpc <65555326+Ferhpc@users.noreply.github.com>
2020-07-09 16:29:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php 
 
 require 'admin/config.php';
+require 'functions.php';
+
+$conexion = conexion($bd_config);
+if (!$conexion){
+  header('Location:error.php');
+}
+
+obtener_post();
+
+
 require 'views/index.view.php';
 
 ?>
